fe74568965f341899b10a343a7100eaf259b5b28
KrivonosPaul <fop_krivonos@ukr.net>
2015-07-24 00:28:55 +0300

    added some #includes

--- HouseCalculator/HouseCalculator/HouseCalculator.cpp
+++ HouseCalculator/HouseCalculator/HouseCalculator.cpp
@@ -2,10 +2,26 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include<time.h>
+#include<windows.h>
+#include <string>
+#include <cmath>
+#include <vector>//для использования векторов (использеутся вместе с std)
+#include <conio.h>
+#include <glut.h>
+#include <gl\GL.h>//не всегда нужны
+#include <gl\GLU.h>//не всегда нужны
+#include <stdlib.h>//узнать для чего нужен
+#include <glaux.h>
+
+using namespace std;
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+
+	_gettch();
 	return 0;
 }
 
